f771656774eb65430944366b623d036281c64dc4
Á¤½ģ <masa10269@gmail.com>
2020-05-23 10:22:03 +0800

    first

---
+++ Assets/Materials/Shaders/Contour/contour.shader
@@ -0,0 +1,85 @@
+﻿Shader "MyShader/countour"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+        _MyCameraPosition("Camera Potition", Vector) = (0, 0, 0, 0)
+        _Threshold("Contour Threshold", Float) = 0.5
+    }
+    SubShader
+    {
+        Tags { "RenderType" = "Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float3 normal : NORMAL;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 contourParam : TEXCOORD1;
+            };
+
+            sampler2D _MainTex;
+            float _Threshold;
+            float4 _MainTex_ST;
+            float4 _MyCameraPosition;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+                // inverse view direction
+                float3 viewDir = normalize(_MyCameraPosition - v.vertex);
+                // world normal
+                float3 worldNormal = UnityObjectToWorldNormal(v.normal);
+
+                o.contourParam = float3(1, 1, 1);
+                // x : normal_dot_view
+                // y : t_kr
+                // z : t_dwkr
+                float normal_dot_view = dot(worldNormal, viewDir);
+                o.contourParam.x = abs(normal_dot_view);
+                //if (normal_dot_view >= 0)
+                //{
+                //    o.contourParam.x = normal_dot_view;
+                //    if (normal_dot_view > _Threshold)
+                //    {
+                //        // calculate
+                //        ;
+                //    }
+                //}
+
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = fixed4(0,0,0,0);
+                if (i.contourParam.x < _Threshold)
+                    col = fixed4(1, 0, 0, 1);
+                else
+                    col = tex2D(_MainTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scripts/Contour/Contour.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+//[ExecuteInEditMode]
+public class Contour : MonoBehaviour
+{
+    public Camera _myCamera;
+    public GameObject _renderObject;
+
+    public RenderTexture renTexture;
+
+    public Renderer cornerRenderer;
+    [Range(0, 31)]
+    public int layer = 31;
+    public int renderWidth = 512;
+
+    bool flag = false;
+    void Update()
+    {
+        //if (_myCamera)
+        //{
+        //    Material mat = transform.GetComponent<Renderer>().sharedMaterial;
+        //    mat.SetVector("_MyCameraPosition", _myCamera.transform.position);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.A))
+            flag = true;
+        else if(Input.GetKeyDown(KeyCode.B))
+            flag = false;
+
+        if (flag)
+        {
+            AddBuffer();
+        }
+        else
+        {
+            _myCamera.RemoveAllCommandBuffers();
+        }
+    }
+
+    void AddBuffer()
+    {
+        _myCamera.RemoveAllCommandBuffers();
+
+        //Shader cornerSahder = Shader.Find("MyShader/valley");
+        //Material cornerMaterial = new Material(cornerSahder);
+        //cornerMaterial.hideFlags = HideFlags.HideAndDontSave;
+
+        cornerRenderer.material.SetVector("_MyCameraPosition", _myCamera.transform.position);
+
+        CommandBuffer commandBuffer = new CommandBuffer();
+
+        int valleyID = Shader.PropertyToID("_ValleyTexture");
+
+        commandBuffer.GetTemporaryRT(valleyID, renderWidth, renderWidth, 0, FilterMode.Point, RenderTextureFormat.RFloat);
+        commandBuffer.SetRenderTarget(valleyID);
+        commandBuffer.ClearRenderTarget(true, true, Color.white, 1f);
+        commandBuffer.DrawRenderer(cornerRenderer, cornerRenderer.material, 0, 0);
+        commandBuffer.SetGlobalTexture("_ValleyTexture", valleyID);
+
+        commandBuffer.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+        commandBuffer.DrawRenderer(cornerRenderer, cornerRenderer.material, 0, 1);
+
+        //commandBuffer.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+        //commandBuffer.DrawRenderer(cornerRenderer, cornerRenderer.material, 0, 0);
+
+        commandBuffer.ReleaseTemporaryRT(valleyID);
+
+        _myCamera.AddCommandBuffer(CameraEvent.AfterForwardAlpha, commandBuffer);
+    }
+
+    void RenderValley()
+    {
+        int oriLayer = _renderObject.layer;
+        // change render object to render layer
+        _renderObject.layer = layer;
+        // create temporary camera
+        GameObject tmpCamObject = new GameObject("Temp Cam Object");
+        tmpCamObject.transform.position = transform.position;
+        Camera tmpCam = tmpCamObject.AddComponent<Camera>();
+        //tmpCam.targetTexture = RenderTexture.GetTemporary(renderWidth, renderWidth, 16);
+        tmpCam.targetTexture = renTexture;
+        tmpCam.cullingMask = layer;
+        tmpCam.RenderWithShader(Shader.Find("MyShader/valley"), "Opaque");
+        tmpCam.Render();
+
+        DestroyImmediate(tmpCamObject);
+        // to origin layer
+        _renderObject.layer = oriLayer;
+    }
+}
